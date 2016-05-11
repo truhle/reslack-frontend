bc8058b2e1ca1aede9c990acac986a7edd736fe4
Todd Ruhlen <rhythmtome@yahoo.com>
2016-05-11 11:54:16 -0400

    Add layout for channel switcher with headers

--- slackc.jsx
+++ slackc.jsx
@@ -2,6 +2,55 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/style.scss';
 
-const HelloWorld = () => ( <div>Hello World!</div> );
+const App = () => ( 
+  <div>
+    <ChannelSwitcher />
+  </div> );
 
-ReactDOM.render(<HelloWorld />, document.getElementById('main'));
+const ChannelSwitcher = React.createClass({
+  render() {
+    return <div className="channel-switcher">
+      <CSHeader />
+      <ChannelsContainer />
+    </div>
+  }
+});
+
+const CSHeader = React.createClass({
+  render() {
+    return <div className="cs-header">
+      MetaTree
+    </div>
+  }
+});
+
+const ChannelsContainer = React.createClass({
+  render() {
+    return <div className="channels-container">
+      <ChannelsHeader name="channels" />
+      <ChannelsHeader name="direct messages" />
+    </div>
+  }
+});
+
+const ChannelsHeader = React.createClass({
+  render() {
+    return <div className="channels-header">
+      <span className="channels-header-name">{this.props.name}</span>
+      <ChannelsCount />
+      <div className="plus-circle">+</div>
+    </div> 
+  }
+});
+
+const ChannelsCount = React.createClass({
+  render() {
+    return <div className="channels-count">
+      (2)
+    </div>
+  }
+});
+
+
+
+ReactDOM.render(<App />, document.getElementById('main'));
