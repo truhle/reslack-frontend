7706c7383d9e8667b9d19fe1af7368581b7adfdd
Todd Ruhlen <rhythmtome@yahoo.com>
2016-05-12 13:44:04 -0400

    Add direct message channel layout

--- slackc.jsx
+++ slackc.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import './styles/_normalize.scss';
 import './styles/style.scss';
 
 const App = () => ( 
@@ -12,13 +13,14 @@ const ChannelSwitcher = React.createClass({
     return <div className="channel-switcher">
       <CSHeader />
       <ChannelsContainer />
+      <DMChannelsContainer />
     </div>
   }
 });
 
 const CSHeader = React.createClass({
   render() {
-    return <div className="cs-header">
+    return <div className="cs-header overflow-ellipses">
       MetaTree
     </div>
   }
@@ -28,16 +30,30 @@ const ChannelsContainer = React.createClass({
   render() {
     return <div className="channels-container">
       <ChannelsHeader name="channels" />
+    </div>
+  }
+});
+
+const DMChannelsContainer = React.createClass({
+  render() {
+    return <div className="channels-container">
       <ChannelsHeader name="direct messages" />
+      <div className="clear"></div>
+      <ul>
+        <DMContainer present="true" username="Taliesin" />
+        <DMContainer present="false" username="Bob" />
+      </ul>
     </div>
   }
 });
 
 const ChannelsHeader = React.createClass({
   render() {
-    return <div className="channels-header">
-      <span className="channels-header-name">{this.props.name}</span>
-      <ChannelsCount />
+    return <div>
+      <div className="channels-header">
+        <span className="channels-header-name overflow-ellipses">{this.props.name}</span>
+        <ChannelsCount />
+      </div>
       <div className="plus-circle">+</div>
     </div> 
   }
@@ -47,6 +63,46 @@ const ChannelsCount = React.createClass({
   render() {
     return <div className="channels-count">
       (2)
+    </div>
+  }
+});
+
+const DMContainer = React.createClass({
+  getInitialState() {
+    return {hidden: "hidden"};
+  },
+
+  addXCircle(e) {
+    this.setState({hidden: ""});
+  },
+  
+  removeXCircle(e) {
+    this.setState({hidden: "hidden"});
+  },
+  
+  render() {
+    return <li className="dm-container"
+               onMouseOver={this.addXCircle}
+               onMouseOut={this.removeXCircle}>
+      <PresenceIndicator present={this.props.present} />
+      <DMUsername present={this.props.present}     
+                  username={this.props.username} />
+      <div className={"x-circle " + this.state.hidden}>x</div>
+    </li>
+  }  
+});
+
+const DMUsername = React.createClass({
+  render() {
+    return <span className={"dm-username overflow-ellipses no-italic-" + this.props.present}>
+      {this.props.username}
+    </span>
+  }
+});
+
+const PresenceIndicator = React.createClass({
+  render() {
+    return <div className={"presence presence-" + this.props.present}>
     </div>
   }
 });
