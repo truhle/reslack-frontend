06b230c9cec259d3be63f65fa8bf98ba788554ae
Todd Ruhlen <rhythmtome@yahoo.com>
2016-07-17 19:13:23 -0400

    Build sign-up page layout

--- slackc.jsx
+++ slackc.jsx
@@ -312,4 +312,42 @@ const App = React.createClass({
   }
 }); 
 
-ReactDOM.render(<App />, document.getElementById('main'));
+
+const SignUp = React.createClass({
+  render() {
+    return <div className="page">
+      <SignUpForm />
+    </div>
+  }
+});
+
+const SignUpForm = React.createClass({
+  render() {
+    return <div className="page-contents">
+      <div className="sign-up-form-card">
+        <h1 className="margin-bottom">
+          Sign in to reslack.net/webschool
+        </h1>
+        <div className="sign-up-form">
+          <p className="margin-bottom">
+            Enter your <strong>email address</strong> and <strong>password</strong>
+            .
+          </p>
+          <p>
+            <input type="email" placeholder="you@domain.com" size="40"></input>
+          </p>
+          <p>
+            <input type="password" placeholder="password" size="40"></input>
+          </p>
+          <p>
+            <div className="btn">Sign in</div>
+          </p>
+        </div>
+
+        
+      </div>
+    </div>
+  }
+});
+
+ReactDOM.render(<SignUp />, document.getElementById('main'));
